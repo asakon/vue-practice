012679b512415e3016bf285bd03a431ca1a0a48a
Asako Kondo <kondoasako@gmail.com>
2021-07-09 19:33:54 +0900

    メソッド

--- chap02/js/hello.js
+++ chap02/js/hello.js
@@ -4,11 +4,23 @@ let app = new Vue ({
 		message: 'みなさん、こんにちは',
 		url: 'https://wings.msn.to',
 		flag: true,
-		email: 'Y-Suzuki@example.com'
+		email: 'Y-Suzuki@example.com',
+		current: new Date().toLocaleString()
+	},
+	computed: {
+		randomc: function() {
+			return Math.random();
+		}
 	},
 	methods: {
 		localEmail: function() {
 			return this.email.split('@')[0].toLowerCase();
+		},
+		onclick: function() {
+			this.current = new Date().toLocaleString();
+		},
+		randomm: function() {
+			return Math.random();
 		}
 	}
 });
